e5563331b92283f2d97b6c179c1ae9884571bd48
vladukkk <vladuslavantonyak94@gmail.com>
2025-06-18 17:13:59 +0300

    Enhance MainWindow UI with custom title bar and controls

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,45 +1,49 @@
 ﻿using PassNester.ViewModels;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace PassNester
+namespace PassNester;
+
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-            var vm = new MainViewModel();
-            vm.RequestAddCollectionDialog = ShowAddCollectionDialog;
-            vm.RequestAddEntryDialog = ShowAddEntryDialog;
-            DataContext = vm;
-        }
+        InitializeComponent();
+        var vm = new MainViewModel();
+        vm.RequestAddCollectionDialog = ShowAddCollectionDialog;
+        vm.RequestAddEntryDialog = ShowAddEntryDialog;
+        DataContext = vm;
+    }
 
-        private (string? key, string? value)? ShowAddEntryDialog()
-        {
-            var dlg = new AddEntryWindow { Owner = this };
-            if (dlg.ShowDialog() == true)
-                return (dlg.Key, dlg.Value);
-            return null;
-        }
+    private (string? key, string? value)? ShowAddEntryDialog()
+    {
+        var dlg = new AddEntryWindow { Owner = this };
+        if (dlg.ShowDialog() == true)
+            return (dlg.Key, dlg.Value);
+        return null;
+    }
 
-        private (string? name, string? color)? ShowAddCollectionDialog()
+    private (string? name, string? color)? ShowAddCollectionDialog()
+    {
+        var dlg = new AddCollectionWindow { Owner = this };
+        if (dlg.ShowDialog() == true)
+            return (dlg.CollectionName, dlg.CollectionColor);
+        return null;
+    }
+
+    private void TitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ClickCount == 2)
         {
-            var dlg = new AddCollectionWindow { Owner = this };
-            if (dlg.ShowDialog() == true)
-                return (dlg.CollectionName, dlg.CollectionColor);
-            return null;
+            WindowState = WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+        }
+        else
+        {
+            DragMove();
         }
     }
+
+    private void Minimize_Click(object sender, RoutedEventArgs e) => WindowState = WindowState.Minimized;
+    private void Maximize_Click(object sender, RoutedEventArgs e) => WindowState = WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+    private void Close_Click(object sender, RoutedEventArgs e) => Close();
 }
